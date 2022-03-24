15189b4942c469314f0919db9d767ebc891bdcae
NickName73 <egorooz73@gmail.com>
2022-03-24 12:53:50 +0300

    Update

--- Generator/DocType.cs
+++ Generator/DocType.cs
@@ -1,33 +1,60 @@
 ﻿using System;
 using System.Linq;
+using SType = System.Type;
 
 public class DocType
 {
-    public Type Type { get; private set; }
-    public DocType(Type type) => this.Type = type;
+    public SType Type { get; private set; }
+    public DocType(SType type) => this.Type = type;
+    public string GenerateIncode()
+    {
+        return GenerateModifers() + GetNameWithGenerics(this.Type);
+    }
+    public string GenerateModifers()
+    {
+        string text = "";
+        text += Type.IsNotPublic ? "private " : "";
+        text += Type.IsPublic ? "public " : "";
+        if (Type.IsInterface)
+        {
+            text += "interface ";
+        }
+        else
+        {
+            text += Type.IsSealed ? "sealed " : "";
+            text += Type.IsAbstract ? "abstract " : "";
+            text += Type.IsValueType ? "struct " : "class ";
+        }
+        return text;
+    }
     public string Namespace => this.Type?.Namespace ?? "";
     public string ClearName => GetNameByType(this.Type);
-    public Template TemplateType {
-        get {
+    public string Name => GetNameByType(this.Type);
+    public string FullClearName => $"{Namespace}.{ClearName}";
+    public string FullName => $"{Namespace}.{Name}";
+    public Template TemplateType
+    {
+        get
+        {
             if (Type.IsInterface) return Template.Interface;
             if (Type.IsValueType) return Template.Struct;
             if (Type.IsEnum) return Template.Enum;
             return Template.Class;
-        } 
+        }
     }
-
-    public static string GetNameByType(Type type)
+    public static string GetNameWithGenerics(SType type)
     {
         if (type.IsGenericType)
         {
-            return String.Concat(type.Name.Split('`').SkipLast(1)) + GetGenericNames(type.GetGenericArguments());
+            return type.Name + GetGenericNames(type.GenericTypeArguments);
         }
         else
         {
             return type.Name;
         }
     }
-    public static string GetGenericNames(Type[] generics)
+    public static string GetNameByType(SType type) => type.Name;
+    public static string GetGenericNames(SType[] generics)
     {
         string text = "";
         foreach (var type in generics)
--- Generator/Entry.cs
+++ Generator/Entry.cs
@@ -5,21 +5,27 @@ using NiTiS.Additions;
 using NiTiS.IO;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Reflection;
 
 public static class Entry
 {
+	public static readonly Dictionary<string, DocType> DocTypes = new();
 	public static void Main() {
+		//Entry logging
 		Console.WriteLine("NiTiS Core Lib V:" + NiTiSCoreLib.BasicLibs[0].GetName().Version);
 		Console.WriteLine("Date Time " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+		//Setup working directories
 		Directory dir = Directory.GetCurrentDirectory();
-		Console.WriteLine($"Global directory are: {dir}");
 		Directory docs = new(System.IO.Path.Combine(dir.Path, "docs"));
 		Directory templates = new(dir.Path, "templates");
+		Console.WriteLine($"Global directory are: {dir}");
+
 		foreach(var asm in NiTiSCoreLib.BasicLibs) {
 			foreach(var type in asm.GetTypes()) {
 				if (type.FullName.StartsWith("System") || type.FullName.StartsWith("Microsoft")) continue; //Skip internal classes
 				if (type.FullName.Contains("__") || type.FullName.Contains('+')) continue; //Skip display classes
 				if (type == typeof(NiTiSCoreLib)) continue;
+				
 				DocType dtype = new(type);
 				string path = dtype.Namespace.Replace('.', '/');
 				File docFile = new(dir, System.IO.Path.Combine(docs.Path, path, dtype.ClearName + ".md"));
@@ -28,8 +34,9 @@ public static class Entry
 				string doc = template.ReadText();
 				Dictionary<string, string> dict = new()
 				{
-					["MEMBER_NAME"] = dtype.ClearName,
 					["MEMBER_NAMESPACE"] = dtype.Namespace,
+					["MEMBER_NAME"] = dtype.ClearName,
+					["MEMBER_INCODE"] = dtype.GenerateIncode(),
 					["MEMBER_ASSEMBLY"] = dtype.Type.Assembly.GetName().Name + " V:" + dtype.Type.Assembly.GetName().Version,
 				};
                 foreach (var repl in dict)
@@ -40,24 +47,6 @@ public static class Entry
 				docFile.Create();
 				docFile.WriteText(doc);
 			}
-			
 		}
 	}
-	/*public static string GetTitleText(Type type)
-    {
-		string text = "";
-		text += type.IsNotPublic ? "private " : "";
-		text += type.IsPublic ? "public " : "";
-		if (type.IsInterface)
-		{
-			text += "interface ";
-		}
-        else
-        {
-			text += type.IsSealed ? "sealed " : "";
-			text += type.IsAbstract ? "abstract " : "";
-			text += type.IsValueType ? "struct " : "class ";
-		}
-		return text + GetNameByType(type);
-    }*/
 }
